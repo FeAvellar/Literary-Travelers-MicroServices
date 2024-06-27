24f99ad9d12bd0508edf12613d2b266937875561
FeAvellar <fe.avellar91@gmail.com>
2024-06-27 01:50:04 -0300

    Insert inicial permissões

---
+++ literarytravelers-user/src/main/resources/db/migration/V7__Insert_initial_permissions.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO tb_role (role_name, description) VALUES
+('ADMIN_ACCESS', 'Permissão para administradores'),
+('FREE_USER_ACCESS', 'Permissão para usuários gratuitos'),
+('PAID_USER_ACCESS', 'Permissão para usuários pagantes');
