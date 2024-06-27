713750eed028b7280a62cafb2834cd15f6b9bb67
FeAvellar <fe.avellar91@gmail.com>
2024-06-27 01:44:35 -0300

    Classe de permissões - controller

---
+++ literarytravelers-user/src/main/java/com/literarytravelers/user/controller/RoleController.java
@@ -0,0 +1,30 @@
+package com.literarytravelers.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.literarytravelers.user.entities.Role;
+import com.literarytravelers.user.service.RoleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RequestMapping
+@RestController
+
+public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping("/role")
+    public ResponseEntity<Void> addRole(@RequestBody Role role) {
+         roleService.addRole(role);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    
+    
+}
