41f8a93eaae733ea53c403dc17fbc90f2e300446
FeAvellar <fe.avellar91@gmail.com>
2024-07-02 23:21:22 -0300

    Atualização

--- literarytravelers-user/src/main/java/com/literarytravelers/user/controller/RoleController.java
+++ literarytravelers-user/src/main/java/com/literarytravelers/user/controller/RoleController.java
@@ -3,6 +3,7 @@ package com.literarytravelers.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class RoleController {
     private RoleService roleService;
 
     @PostMapping("/role")
-    public ResponseEntity<Void> addRole(@RequestBody Role role) {
+    public ResponseEntity<Void> addRole(@Validated @RequestBody Role role) {
          roleService.addRole(role);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
