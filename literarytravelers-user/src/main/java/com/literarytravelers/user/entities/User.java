5756ca74e69f7b1cc065011028fc8308b841ab47
FeAvellar <fe.avellar91@gmail.com>
2024-07-02 23:22:29 -0300

    Add validações

--- literarytravelers-user/src/main/java/com/literarytravelers/user/entities/User.java
+++ literarytravelers-user/src/main/java/com/literarytravelers/user/entities/User.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -22,12 +23,19 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
-@Table(name= "tb_user")
+@Table(name = "tb_user")
 @Data
-public class User implements UserDetails{
+public class User implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -35,38 +43,59 @@ public class User implements UserDetails{
     private Long id;
 
     @Column(nullable = false, length = 100)
-    private String fullname; //Nome completo
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 255, message = "Name must be less than 255 characters")
+    @Pattern(regexp = "^[A-Z]+(.)*", message = "Name must contain only letters and spaces")
+    private String fullname; // Nome completo
 
     @Column(nullable = false, length = 100)
+    @NotBlank(message = "Login name is mandatory")
+    @Size(min = 5, max = 50, message = "Login name must be between 5 and 15 characters")
+    @Pattern(regexp = "^[A-Z0-9]+$", message = "O loginname deve conter apenas letras maiúsculas e números")
     private String loginName;
 
+    public void setloginName(String loginName) {
+        this.loginName = loginName != null ? loginName.toUpperCase() : null;  //Serve para deixar todas as letras do login name maiusculas
+    }
+
     @Column(nullable = false)
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Size(max = 255, message = "Email must be less than 255 characters")
     private String email;
-    
+
     @Column(nullable = false, unique = true)
+    @CPF(message = "Invalid CPF")
     private String cpf;
 
     @Column(nullable = false, unique = false)
+    @NotNull(message = "Birth date is mandatory")
+    @Past(message = "Birth date must be in the past")
     private Date birthDate;
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "tb_user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @Valid
     private List<Role> roles;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
+    @Valid
     private List<Phone> phones;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
+    @Valid
     private List<Address> address;
-
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
+    @Valid
     private Photo photo;
 
     // Métodos UserDetails
@@ -98,7 +127,7 @@ public class User implements UserDetails{
 
     @Override
     public String getUsername() {
-       return loginName;
+        return loginName;
     }
 
 }
